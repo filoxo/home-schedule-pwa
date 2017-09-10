b08cf815c6519b5bef4ba33fb313bb4577488c25
Carlos Filoteo <c.filoteo@live.com>
2017-09-10 16:48:32 -0600

    Add tabs, route definitions

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,10 @@
 import { h, Component } from 'preact'
-import { Router } from 'preact-router'
+import { Router, route, Link } from 'preact-router'
 
-import Header from './header'
-import Home from '../routes/home'
-import Profile from '../routes/profile'
-// import Home from 'async!./home';
-// import Profile from 'async!./profile';
-
-import Drawer from 'preact-material-components/Drawer'
-import List from 'preact-material-components/List'
 import Toolbar from 'preact-material-components/Toolbar'
 import 'preact-material-components/Toolbar/style.css'
-import 'preact-material-components/Drawer/style.css'
-import 'preact-material-components/List/style.css'
+import Tabs from 'preact-material-components/Tabs'
+import 'preact-material-components/Tabs/style.css'
 
 export default class App extends Component {
   /** Gets fired when the route changes.
@@ -30,37 +22,24 @@ export default class App extends Component {
   render() {
     return (
       <div id="app">
-        <Drawer.TemporaryDrawer
-          ref={drawer => {
-            this.drawer = drawer
-          }}
-        >
-          <Drawer.TemporaryDrawerHeader className="mdc-theme--primary-bg">
-            Components
-          </Drawer.TemporaryDrawerHeader>
-          <Drawer.TemporaryDrawerContent>
-            <List>
-              <List.LinkItem>Home</List.LinkItem>
-              <List.LinkItem onClick={this.toggleDrawer}>
-                CLose menu
-              </List.LinkItem>
-            </List>
-          </Drawer.TemporaryDrawerContent>
-        </Drawer.TemporaryDrawer>
         <Toolbar className="toolbar">
           <Toolbar.Row>
             <Toolbar.Section align-start={true}>
-              <Toolbar.Icon menu={true} onClick={this.toggleDrawer}>
-                menu
-              </Toolbar.Icon>
-              <Toolbar.Title>My App</Toolbar.Title>
+              <Toolbar.Title>Cleaning Schedule</Toolbar.Title>
             </Toolbar.Section>
           </Toolbar.Row>
         </Toolbar>
+        <Tabs indicator-accent={true}>
+          <Tabs.Tab href="/daily">Daily</Tabs.Tab>
+          <Tabs.Tab href="/weekly">Weekly</Tabs.Tab>
+          <Tabs.Tab href="/monthly">Monthly</Tabs.Tab>
+        </Tabs>
         <Router onChange={this.handleRoute}>
-          <Home path="/" />
-          <Profile path="/profile/" user="me" />
-          <Profile path="/profile/:user" />
+          <div path="/daily" default>
+            daily
+          </div>
+          <div path="/weekly">weekly</div>
+          <div path="/monthly">monthly</div>
         </Router>
       </div>
     )
