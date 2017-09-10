5c0148034b6bd08c9bc4f6fc55094fb56170dccd
Carlos Filoteo <c.filoteo@live.com>
2017-09-10 12:27:39 -0600

    Add Drawer and Toolbar to app

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,13 @@ import Profile from '../routes/profile'
 // import Home from 'async!./home';
 // import Profile from 'async!./profile';
 
+import Drawer from 'preact-material-components/Drawer'
+import List from 'preact-material-components/List'
+import Toolbar from 'preact-material-components/Toolbar'
+import 'preact-material-components/Toolbar/style.css'
+import 'preact-material-components/Drawer/style.css'
+import 'preact-material-components/List/style.css'
+
 export default class App extends Component {
   /** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
@@ -16,10 +23,40 @@ export default class App extends Component {
     this.currentUrl = e.url
   }
 
+  toggleDrawer = () => {
+    this.drawer.MDComponent.open = !this.drawer.MDComponent.open
+  }
+
   render() {
     return (
       <div id="app">
-        <Header />
+        <Drawer.TemporaryDrawer
+          ref={drawer => {
+            this.drawer = drawer
+          }}
+        >
+          <Drawer.TemporaryDrawerHeader className="mdc-theme--primary-bg">
+            Components
+          </Drawer.TemporaryDrawerHeader>
+          <Drawer.TemporaryDrawerContent>
+            <List>
+              <List.LinkItem>Home</List.LinkItem>
+              <List.LinkItem onClick={this.toggleDrawer}>
+                CLose menu
+              </List.LinkItem>
+            </List>
+          </Drawer.TemporaryDrawerContent>
+        </Drawer.TemporaryDrawer>
+        <Toolbar className="toolbar">
+          <Toolbar.Row>
+            <Toolbar.Section align-start={true}>
+              <Toolbar.Icon menu={true} onClick={this.toggleDrawer}>
+                menu
+              </Toolbar.Icon>
+              <Toolbar.Title>My App</Toolbar.Title>
+            </Toolbar.Section>
+          </Toolbar.Row>
+        </Toolbar>
         <Router onChange={this.handleRoute}>
           <Home path="/" />
           <Profile path="/profile/" user="me" />
