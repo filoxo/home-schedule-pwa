a5b1aafa4100b6766ae8fc5d1946135faa66bd85
Carlos Filoteo <c.filoteo@live.com>
2017-09-17 23:02:35 -0600

    Add chores data source

---
+++ src/chores.js
@@ -0,0 +1,35 @@
+export default {
+  daily: [
+    { text: 'Empty kitchen sink', status: false },
+    { text: 'Wipe down counters', status: false },
+    { text: 'Clear off kitchen table', status: false },
+    { text: 'Wipe kitchen table', status: false },
+    { text: 'Pick up toys', status: false },
+    { text: 'Declutter main living areas', status: false }
+  ],
+  weekly: [
+    { text: 'Mon: Clean out fridge', status: false },
+    { text: 'Tue: Tidy up', status: false },
+    { text: 'Tue: Tidy up', status: false },
+    { text: 'Wed: Wash - darks', status: false },
+    { text: 'Wed: Wash - lights', status: false },
+    { text: 'Wed: Wash - baby', status: false },
+    { text: 'Thu: Dusting furniture', status: false },
+    { text: 'Fri: Floors - sweep', status: false },
+    { text: 'Fri: Floors - mop', status: false },
+    { text: 'Fri: Floors - vacuum', status: false },
+    { text: 'Sat: Sinks', status: false },
+    { text: 'Sat: Showers', status: false }
+  ],
+  monthly: [
+    { text: 'Wipe/magic erase walls', status: false },
+    {
+      text:
+        'Go through clothes/shoes/underwear and get rid of too old/small stuff',
+      status: false
+    },
+    { text: 'Clean baseboards', status: false },
+    { text: 'Dust blinds', status: false },
+    { text: 'Clean windows', status: false }
+  ]
+}
