fad9cf70c8af0e45c03a370870a23c7f10de14c0
Carlos Filoteo <c.filoteo@live.com>
2017-09-17 21:35:07 -0600

    Add toggleTask method to update parent state

--- src/routes/choreList/index.js
+++ src/routes/choreList/index.js
@@ -7,6 +7,11 @@ import FormField from 'preact-material-components/FormField'
 import 'preact-material-components/FormField/style.css'
 
 export default class ChoreList extends Component {
+  toggleTask = e => {
+    const index = parseInt(e.target.dataset.index, 10)
+    this.props.list[index].status = !this.props.list[index].status
+    this.props.update(this.props.list)
+  }
   render() {
     return (
       <List>
@@ -15,7 +20,12 @@ export default class ChoreList extends Component {
           return (
             <List.Item key={id}>
               <FormField>
-                <Checkbox id={id} checked={status} />
+                <Checkbox
+                  id={id}
+                  checked={status}
+                  onClick={this.toggleTask}
+                  data-index={i}
+                />
                 <label for={id}>{text}</label>
               </FormField>
             </List.Item>
